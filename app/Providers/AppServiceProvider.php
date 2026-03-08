076bae209441085641a02a38628b0a1e6e4b38de
izigower <hugokemaies@gmail.com>
2026-03-08 18:48:15 +0100

    fix(database): migrate in-memory sqlite db on vercel deployment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +22,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         URL::forceScheme('https');
+
+        if (env('DB_CONNECTION') === 'sqlite' && env('DB_DATABASE') === ':memory:') {
+            Artisan::call('migrate', ['--force' => true]);
+        }
     }
 }
